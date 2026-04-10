25b2b644c8a8aba7ff353ce555d01ebfdfb2d443
Your Name <you@example.com>
2026-04-10 11:25:02 -0300

    Subo unidad 2

---
+++ src/Ejercicio7/Ejercicio7.java
@@ -0,0 +1,48 @@
+package Ejercicio7;
+
+import java.util.Scanner;
+
+public class Ejercicio7 {
+
+	public static void main(String[] args) {
+		
+	Scanner entrada = new Scanner (System.in);
+	
+	System.out.println("Ingrese la cantidad de productos: ");
+	int productos = entrada.nextInt();
+	
+	
+	String nombrep;
+	int cantp;
+	
+	for ( int i = 0; i < productos; i++ ) {
+		System.out.println("Ingrese nombre del producto N° " + (i+1));
+		nombrep = entrada.nextLine();
+		System.out.println("Ingrese la cantidad de unidades del producto N° " + (i+1));
+		cantp = entrada.nextInt();
+							
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+		
+        
+        
+        
+        
+        
+		
+		
+		
+		
+		
+	}
+
+}
